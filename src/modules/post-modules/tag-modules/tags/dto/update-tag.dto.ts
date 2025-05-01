65a748750453d24484fe65b8f5cee0e44e3dc9e7
Jaime <huaychojaime@gmail.com>
2025-05-01 00:19:55 -0400

    Avances en tags

--- src/modules/post-modules/tag-modules/tags/dto/update-tag.dto.ts
+++ src/modules/post-modules/tag-modules/tags/dto/update-tag.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTagDto } from './create-tag.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateTagDto extends PartialType(CreateTagDto) {}
+export class UpdateTagDto {
+    @ApiProperty({description: 'Nuevo nombre de la etiqueta',type: String,nullable: false})
+    name: string
+}
--- src/modules/post-modules/tag-modules/tags/tags.controller.ts
+++ src/modules/post-modules/tag-modules/tags/tags.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, Put } from '@nestjs/common';
 import { TagsService } from './services/tags.service';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
@@ -6,6 +6,7 @@ import { Response } from 'express';
 import { responseError } from 'src/common/helpers/out.helper';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { GetTagsDto } from './dto/get-tags.dto';
+import { swaggerRes404 } from 'src/common/helpers/classes.dto';
 
 @Controller('tags')
 export class TagsController {
@@ -25,12 +26,13 @@ export class TagsController {
 	}
 
 	@Get()
-	@ApiOperation({summary: 'Apipara obtener las etiquetas de testimonios'})
+	@ApiOperation({summary: 'Api para obtener las etiquetas de testimonios'})
 	@ApiResponse({
 		description: 'Salida en caso de obtener exitosamente todos las etiquetas',
 		status: 200,
 		type: GetTagsDto
 	})
+	@ApiOperation(swaggerRes404())
 	async findAll(@Res() res: Response) {
 		try {
 			const tags = await this.tagsService.findAll();
@@ -56,9 +58,10 @@ export class TagsController {
 		}
 	}
 
-	@Patch(':id')
-	update(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto) {
-		return this.tagsService.update(+id, updateTagDto);
+	@Put(':id')
+	@ApiOperation({summary: 'Api para actualizar una etiqueta'})
+	update(@Param('id') id: string, @Body() data: UpdateTagDto) {
+		return this.tagsService.update(parseInt(id), data);
 	}
 
 	@Delete(':id')
