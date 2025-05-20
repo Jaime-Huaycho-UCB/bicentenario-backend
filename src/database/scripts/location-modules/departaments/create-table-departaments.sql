54845d4311b91800a5f7a1c84dc80ae2dd279eaf
Jaime <huaychojaime@gmail.com>
2025-05-20 19:37:50 -0400

    Creacion de mas columnas para lla longitud y latitud de un depertamento y correccion de query para el cargado

--- src/database/scripts/location-modules/departaments/create-table-departaments.sql
+++ src/database/scripts/location-modules/departaments/create-table-departaments.sql
@@ -1,5 +1,7 @@
 create table departaments (
     id_departament serial,
     name varchar(100) not null,
+    latitude float not null,
+    longitude float not null,
     primary key (id_departament)
 );
--- src/database/scripts/location-modules/departaments/query-departaments.sql
+++ src/database/scripts/location-modules/departaments/query-departaments.sql
@@ -1,11 +1,11 @@
-INSERT INTO departaments (name) VALUES
-('La Paz'),
-('Santa Cruz'),
-('Cochabamba'),
-('Oruro'),
-('Potosí'),
-('Sucre'),
-('Tarija'),
-('Chuquisaca'),
-('Beni'),
-('Pando');
+INSERT INTO departaments (name, latitude, longitude) VALUES
+('La Paz', -16.5000, -68.1500),
+('Santa Cruz', -17.8000, -63.2000),
+('Cochabamba', -17.3900, -66.1600),
+('Oruro', -17.9833, -68.1667),
+('Potosí', -19.5833, -65.7500),
+('Sucre', -19.0333, -65.2625),
+('Tarija', -21.5333, -64.7333),
+('Chuquisaca', -19.0333, -65.2625),
+('Beni', -14.0000, -65.0000),
+('Pando', -11.1833, -67.1833);
